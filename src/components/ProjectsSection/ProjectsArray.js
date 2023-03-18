c770269ac9c8af2055d7312f4c774fad5f45a27e
Joao Lapas <artesanatoavela@gmail.com>
2023-03-18 08:55:20 +0000

    some actualizations to cv

--- src/components/ProjectsSection/ProjectsArray.js
+++ src/components/ProjectsSection/ProjectsArray.js
@@ -22,38 +22,6 @@ const Cards = [
       longDescription: "example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example ",
       icons: 'kjh'
     },
-    {
-      id: 3,
-      image: project3,
-      title: 'Project 3',
-      description: "example example example example example example example example example",
-      longDescription: "example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example ",
-      icons: 'kjh'
-    },
-    {
-      id: 4,
-      image: project3,
-      title: 'Project 4',
-      description: "example example example example example example example example example",
-      longDescription: "example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example ",
-      icons: 'kjh'
-    },
-    {
-      id: 5,
-      image: project3,
-      title: 'Project 5',
-      description: "example example example example example example example example example",
-      longDescription: "example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example ",
-      icons: 'kjh'
-    },
-    {
-      id: 6,
-      image: project3,
-      title: 'Project 6',
-      description: "example example example example example example example example example",
-      longDescription: "example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example example ",
-      icons: 'kjh'
-    },
     
   ];
 
